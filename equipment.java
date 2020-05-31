70d9cf79e5cf6d25540e31ddd5a9c1db2cd5a5cb
Betul BEIDAS <48389415+betul11@users.noreply.github.com>
2020-05-31 20:45:24 +0300

    Add files via upload

---
+++ equipment.java
@@ -0,0 +1,70 @@
+package sample.model;
+
+public class equipment {
+    private  String equipment;
+    private String poleDistance;
+    private String lightIntensity;
+    private String carrierMedium;
+    private String lightDistance;
+    private String magTech;
+
+    public equipment() {
+    }
+
+    public equipment(String equipment, String poleDistance, String lightIntensity, String carrierMedium, String lightDistance, String magTech) {
+        this.equipment = equipment;
+        this.poleDistance = poleDistance;
+        this.lightIntensity = lightIntensity;
+        this.carrierMedium = carrierMedium;
+        this.lightDistance = lightDistance;
+        this.magTech = magTech;
+    }
+
+    public String getEquipment() {
+        return equipment;
+    }
+
+    public void setEquipment(String equipment) {
+        this.equipment = equipment;
+    }
+
+    public String getPoleDistance() {
+        return poleDistance;
+    }
+
+    public void setPoleDistance(String poleDistance) {
+        this.poleDistance = poleDistance;
+    }
+
+    public String getLightIntensity() {
+        return lightIntensity;
+    }
+
+    public void setLightIntensity(String lightIntensity) {
+        this.lightIntensity = lightIntensity;
+    }
+
+    public String getCarrierMedium() {
+        return carrierMedium;
+    }
+
+    public void setCarrierMedium(String carrierMedium) {
+        this.carrierMedium = carrierMedium;
+    }
+
+    public String getLightDistance() {
+        return lightDistance;
+    }
+
+    public void setLightDistance(String lightDistance) {
+        this.lightDistance = lightDistance;
+    }
+
+    public String getMagTech() {
+        return magTech;
+    }
+
+    public void setMagTech(String magTech) {
+        this.magTech = magTech;
+    }
+}
